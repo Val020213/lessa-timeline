5ac03099294a246db2383a283f9e7aa29d9c191d
osvaldo.rmp <osvaldo.rmp@avangenio.com>
2024-10-04 19:51:22 -0400

    change solution

--- timeline.py
+++ timeline.py
@@ -1,17 +1,61 @@
+# Streamlit Timeline Component Example
+
 import streamlit as st
-from streamlit_timeline import st_timeline
+from streamlit_timeline import timeline
 
-st.set_page_config(layout="wide")
 
-items = [
-    {"id": 1, "content": "2022-10-20", "start": "2022-10-20"},
-    {"id": 2, "content": "2022-10-09", "start": "2022-10-09"},
-    {"id": 3, "content": "2022-10-18", "start": "2022-10-18"},
-    {"id": 4, "content": "2022-10-16", "start": "2022-10-16"},
-    {"id": 5, "content": "2022-10-25", "start": "2022-10-25"},
-    {"id": 6, "content": "2022-10-27", "start": "2022-10-27"},
-]
+exampleJson = {
+    "title": {
+        "media": {
+            "url": "",
+            "caption": " <a target=\"_blank\" href=''>credits</a>",
+            "credit": "",
+        },
+        "text": {
+            "headline": "Welcome to<br>Streamlit Timeline",
+            "text": "<p>A Streamlit Timeline component by integrating TimelineJS from Knightlab</p>",
+        },
+    },
+    "events": [
+        {
+            "media": {
+                "url": "https://vimeo.com/143407878",
+                "caption": "How to Use TimelineJS (<a target=\"_blank\" href='https://timeline.knightlab.com/'>credits</a>)",
+            },
+            "start_date": {"year": "2016", "month": "1"},
+            "text": {
+                "headline": "TimelineJS<br>Easy-to-make, beautiful timelines.",
+                "text": "<p>TimelineJS is a populair tool from Knightlab. It has been used by more than 250,000 people to tell stories seen hundreds of millions of times, and is available in more than sixty languages. </p>",
+            },
+        },
+        {
+            "media": {
+                "url": "https://www.youtube.com/watch?v=CmSKVW1v0xM",
+                "caption": "Streamlit Components (<a target=\"_blank\" href='https://streamlit.io/'>credits</a>)",
+            },
+            "start_date": {"year": "2020", "month": "7", "day": "13"},
+            "text": {
+                "headline": "Streamlit Components<br>version 0.63.0",
+                "text": "Streamlit lets you turn data scripts into sharable web apps in minutes, not weeks. It's all Python, open-source, and free! And once you've created an app you can use our free sharing platform to deploy, manage, and share your app with the world.",
+            },
+        },
+        {
+            "media": {
+                "url": "https://github.com/innerdoc/streamlit-timeline/raw/main/component-logo.png",
+                "caption": "github/innerdoc (<a target=\"_blank\" href='https://www.github.com/innerdoc/'>credits</a>)",
+            },
+            "start_date": {"year": "2021", "month": "2"},
+            "text": {
+                "headline": "Streamlit TimelineJS component",
+                "text": "Started with a demo on https://www.innerdoc.com/nlp-timeline/ . <br>Then made a <a href='https://github.com/innerdoc/streamlit-timeline'>Streamlit component</a> of it. <br>Then made a <a href='https://pypi.org/project/streamlit-timeline/'>PyPi package</a> for it.",
+            },
+        },
+    ],
+}
 
-timeline = st_timeline(items, groups=[], options={}, height="300px")
-st.subheader("Selected item")
-st.write(timeline)
+# use full page width
+st.set_page_config(page_title="Timeline Example", layout="wide")
+
+
+# render timeline
+timeline(exampleJson, height=800)
